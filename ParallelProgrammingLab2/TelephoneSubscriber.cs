08072ae4f2917c1742123d89a6321aad1f6ef376
One290 <lenya.dorovskoy@mail.ru>
2024-02-25 13:54:23 +0800

    Work

---
+++ ParallelProgrammingLab2/TelephoneSubscriber.cs
@@ -0,0 +1,10 @@
+namespace ParallelProgrammingLab2;
+
+public class TelephoneSubscriber
+{
+    public string FullName { get; init; } = string.Empty;
+    
+    public string PhoneNumber { get; init; } = string.Empty;
+    
+    public string Address { get; init; } = string.Empty;
+}
